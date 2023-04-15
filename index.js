d95f55999d97e975fb1ff94784f94b9cd3f8fb2e
DanielRendon1020 <d.danny1020@gmail.com>
2023-04-15 15:11:26 -0700

    stylized 404 page

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).send(`
+    <body style="text-align: center;">
+        <h1 style="font-size: 10rem">404 Page</h1>
+        <img src="https://media.giphy.com/media/0avOIv3VoMz3WcDGXW/giphy.gif">
+    </body>`)
 })
 
 app.listen(process.env.PORT)
