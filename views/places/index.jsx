ed0dfe97ab93fd994661db6d13a2fd6dc7182764
DanielRendon1020 <d.danny1020@gmail.com>
2023-04-19 18:10:42 -0700

    Adding places view with mock data

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,11 +1,20 @@
 const React = require('react')
-const Def = require('./default')
+const Def = require('../default')
 
-function index() {
+function index(data) {
+    let placesFormatted = data.places.map((places) => {
+      return (
+        <div>
+            <h2>{places.name}</h2>
+            <img src="{place.pic}" alt="{place.name}"/>
+        </div>
+      )  
+    })
     return (
         <Def>
             <main>
                 <h1>Places</h1>
+                {placesFormatted}
             </main>
         </Def>
     )
