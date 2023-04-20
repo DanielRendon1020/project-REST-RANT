077152970332c29a4ba6dfc925bcebc8bc6b20a3
DanielRendon1020 <d.danny1020@gmail.com>
2023-04-19 21:13:08 -0700

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+
+//Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -13,11 +17,6 @@ app.get('/', (req, res) => {
 
 app.get('*', (req, res) => {
     res.render('error404')
-    // res.status(404).send(`
-    // <body style="text-align: center;">
-    //     <h1 style="font-size: 10rem">404 Page</h1>
-    //     <img src="https://media.giphy.com/media/0avOIv3VoMz3WcDGXW/giphy.gif">
-    // </body>`)
 })
 
 app.listen(process.env.PORT)
