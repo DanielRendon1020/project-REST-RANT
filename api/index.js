17102e78f49c4ef514a14bcb976f2b08e6b6c7a4
DanielRendon1020 <d.danny1020@gmail.com>
2023-06-07 21:21:32 -0700

    more testing

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(methodOverride('_method'))
 
 
 //Controllers and Routes
-app.use('/places', require('./controllers/places'))
+app.use('/places', require('../controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home')
